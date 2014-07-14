237f2ea294a38f52c5a11f31147a21fbfb9b7630
Cristobal Avila <cavila@spredfast.com>
2014-07-13 20:13:56 -0600

    basic modal added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+console.log('main.js');
+
+console.log($);
+
+$(".add-btn").click(function(){
+    console.log("click");
+
+    $(".didi-modal").modal({
+        backdrop: true,
+        show: true,
+        closable: true
+    });
+    $(".didi-modal").modal("show");
+});
+
+$(".didi-modal-close").click(function() {
+    $(".didi-modal").modal("hide");
+});
