261b13aa56858a789a809ea6aace3fe75a9a9592
Cristobal Avila <cavila@spredfast.com>
2014-07-19 13:44:56 -0600

    map base added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,6 +8,34 @@ $(".add-btn").click(function(){
         closable: true
     });
     $(".didi-modal").modal("show");
+
+// display map
+    (function (){
+        "use strict";
+        google.maps.visualRefresh = true;
+
+        var marker, map,
+            myLocation = {lat:9.9271278, lon:-84.08200999999997},
+            mapOptions = {
+                zoom: 8,
+                center: new google.maps.LatLng(myLocation.lat, myLocation.lon),
+                mapTypeId: google.maps.MapTypeId.PLAN,
+                disableDefaultUI: true
+            };
+
+        $('#map-canvas').css("height", "110px").css("padding", "0px");
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        /** MyPosition **/
+        marker = new google.maps.Marker({
+            map: map,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            position: new google.maps.LatLng(myLocation.lat, myLocation.lon),
+        });
+        console.log("map resize");
+        google.maps.event.trigger(map, "resize");
+    })();
 });
 
 $(".didi-modal-close").click(function() {
@@ -39,3 +67,35 @@ $('.didi-calendar-ico.end').click(function() {
 
 // initialize datepair
 $('.datetimer-input-container-col').datepair();
+
+
+//$('#map-canvas').on('pageshow', function(){
+//    google.maps.event.trigger(canvas-map, "resize");
+//});
+
+//$(document).ready(function(){
+//    "use strict";
+//    google.maps.visualRefresh = true;
+//
+//    var marker, map,
+//        myLocation = {lat:50, lon:-80},
+//        mapOptions = {
+//            zoom: 5,
+//            center: new google.maps.LatLng(myLocation.lat, myLocation.lon),
+//            mapTypeId: google.maps.MapTypeId.PLAN,
+//            disableDefaultUI: true
+//        };
+//
+//    $('#map-canvas').css("height", "200px").css("padding", "0px");
+//    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+//
+//    /** MyPosition **/
+//    marker = new google.maps.Marker({
+//        map: map,
+//        draggable: false,
+//        animation: google.maps.Animation.DROP,
+//        position: new google.maps.LatLng(myLocation.lat, myLocation.lon),
+//    });
+//    console.log("map resize");
+//    google.maps.event.trigger(map, "resize");
+//});
