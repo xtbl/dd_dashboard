1724c21ba0f8ab2612c30fe865b33de685a0789b
Cristobal Avila <cavila@spredfast.com>
2014-07-20 17:46:18 -0600

    add/edit module created

--- app/scripts/modals.js
+++ app/scripts/modals.js
@@ -4,45 +4,88 @@
  *  SearchPlaceMap for map related functionality
  */
 
-$(".add-btn").on("click", function(){
-    $(".didi-modal").modal({
-        backdrop: true,
-        show: true,
-        closable: true
+var DiDiModals = {
+    settings: {
+
+    },
+    addEvent: function (){
+        $(".add-btn").on("click", function(){
+            // change modal header add or edit event
+            $(".add-event-modal .event-modal-header").text("Agregar Evento");
+            $(".didi-modal").modal({
+                backdrop: true,
+                show: true,
+                closable: true
+            });
+            $(".didi-modal").modal("show");
+            console.log(this);
+            console.log($(this).closest(".didi-modal-header h2"));
+
+        //  initialize map, date time controls
+            DiDiSearchPlaceMap.init();
+            eventModalDateTimeInit();
+        });
+
+        $(".didi-modal-close").click(function() {
+            $(".didi-modal").modal("hide");
+        });
+    },
+    editEvent: function (){
+        $(".event-panel .edit-btn").on("click", function(){
+            // change modal header add or edit event
+            $(".add-event-modal .event-modal-header").text("Editar Evento");
+            $(".didi-modal").modal({
+                backdrop: true,
+                show: true,
+                closable: true
+            });
+            $(".didi-modal").modal("show");
+            console.log(this);
+            console.log($(this).closest(".didi-modal-header h2"));
+
+        //  initialize map, date time controls
+            DiDiSearchPlaceMap.init();
+            eventModalDateTimeInit();
+        });
+
+        $(".didi-modal-close").click(function() {
+            $(".didi-modal").modal("hide");
+        });
+    },
+    init: function() {
+        this.addEvent();
+        this.editEvent();
+    }
+};
+
+DiDiModals.init();
+
+
+/**
+ * Date and time inputs in modal
+ */
+function eventModalDateTimeInit () {
+    // initialize input widgets first
+    $('.datetimer-input-container-col .time').timepicker({
+        'showDuration': true,
+        'timeFormat': 'g:ia',
+        'autocomplete': true
     });
-    $(".didi-modal").modal("show");
 
-
-//  initialize map
-    DiDiSearchPlaceMap.init();
-});
-
-$(".didi-modal-close").click(function() {
-    $(".didi-modal").modal("hide");
-});
-
-// date and time in modal
-
-// initialize input widgets first
-$('.datetimer-input-container-col .time').timepicker({
-    'showDuration': true,
-    'timeFormat': 'g:ia',
-    'autocomplete': true
-});
-
-$('.datetimer-input-container-col .date').datepicker({
-    'format': 'd/m/yyyy',
-    'autoclose': true
-});
+    $('.datetimer-input-container-col .date').datepicker({
+        'format': 'd/m/yyyy',
+        'autoclose': true
+    });
 
 // date controls for clickable icon
-$('.didi-calendar-ico.start').click(function() {
-    $('.datetimer-input-container-col .date.start').datepicker('show');
-});
+    $('.didi-calendar-ico.start').click(function() {
+        $('.datetimer-input-container-col .date.start').datepicker('show');
+    });
 
-$('.didi-calendar-ico.end').click(function() {
-    $('.datetimer-input-container-col .date.end').datepicker('show');
-});
+    $('.didi-calendar-ico.end').click(function() {
+        $('.datetimer-input-container-col .date.end').datepicker('show');
+    });
 
 // initialize datepair
-$('.datetimer-input-container-col').datepair();
+    $('.datetimer-input-container-col').datepair();
+};
