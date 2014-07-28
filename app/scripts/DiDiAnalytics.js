d5f104c826400f99cbcfbae2e05edc4785fea5d8
Cristobal Avila <cavila@spredfast.com>
2014-07-27 18:32:31 -0600

    added analytic data selector base

--- app/scripts/DiDiAnalytics.js
+++ app/scripts/DiDiAnalytics.js
@@ -1,6 +1,7 @@
 /**
  * DiDi Analytics Graphs
  * dependencies:
+ *  jQuery
  *  morris.js
  */
 
@@ -10,8 +11,11 @@ var DiDiAnalyticsCharts = {
         barGraphEl: "analytics-grafico-barras",
         colors: ['#93c23e', '#2681a7', '#ba131a', '#808285'] // DiDi colors
     },
+    analyticsLineChart: null,
+    analyticsBarChart: null,
     lineChartInit: function() {
-        Morris.Line({
+        // initializes linechart with default data set (eg: numero de personas)
+        var analyticsLineChart = Morris.Line({
             element: this.settings.lineGraphEl,
             data: [
                 { y: '2006', a: 100, b: 90, c: 40 },
@@ -30,6 +34,17 @@ var DiDiAnalyticsCharts = {
             smooth: false,
             grid: false
         });
+        // assign the line chart object created
+        this.analyticsLineChart = analyticsLineChart;
+        // initializes line chart dropdown controls/selection of chart data
+        var self = this;
+        $("#line-chart-options li a").on("click", function() {
+            var chartDataSelected = $(this).data("lineChartSet");
+            self.setLineChartData(chartDataSelected);
+            var chartDataSelectedText = $(this).text();
+            //update dropdown with selection
+            ($("#line-chart-options").siblings("button.chart-selection-btn")).text(chartDataSelectedText);
+        })
     },
     barChartInit: function() {
         Morris.Bar({
@@ -50,6 +65,30 @@ var DiDiAnalyticsCharts = {
             grid: false
         });
     },
+    setLineChartData: function(selectedDataId) {
+        // get fake dataset
+        var dataSet = getLineChartData();
+        // get data according to selection
+        var selectedData = $.grep(dataSet, function(item){
+            return item.id === selectedDataId;
+        });
+
+        if(selectedData.length){
+            this.analyticsLineChart.setData(selectedData[0].data);
+        }
+    },
+    // request for server data might be here when service is ready
+    getLineChartData: function() {
+        var dataSet = [];
+
+        return dataSet;
+    },
+    createNewLineChart: function(dataSet) {
+
+    },
+    updateBarChartData: function() {
+
+    },
     init: function() {
         console.log("DiDiAnalytics init");
     }
@@ -57,3 +96,95 @@ var DiDiAnalyticsCharts = {
 
 DiDiAnalyticsCharts.lineChartInit();
 DiDiAnalyticsCharts.barChartInit();
+
+
+//    replace with data from server
+function getLineChartData() {
+    var lineChartData = [
+        {
+            id: "numPersonas",
+            name: "# de personas",
+            data: [
+                { y: '2006', a: 100, b: 90, c: 100 },
+                { y: '2007', a: 75, b: 65, c: 100 },
+                { y: '2008', a: 50, b: 40, c: 50 },
+                { y: '2009', a: 75, b: 65, c: 2 },
+                { y: '2010', a: 50, b: 40, c: 100 },
+                { y: '2011', a: 75, b: 65, c: 100 },
+                { y: '2012', a: 100, b: 90, c: 100 }
+            ]
+        },
+        {
+            id: "criterio1",
+            name: "criterio personalizado 1",
+            data: [
+                { y: '2006', a: 100, b: 90, c: 100 },
+                { y: '2007', a: 75, b: 65, c: 100 },
+                { y: '2008', a: 50, b: 40, c: 1 },
+                { y: '2009', a: 75, b: 65, c: 100 },
+                { y: '2010', a: 50, b: 40, c: 100 },
+                { y: '2011', a: 75, b: 65, c: 1 },
+                { y: '2012', a: 100, b: 90, c: 100 }
+            ]
+        },
+        {
+            id: "criterio2",
+            name: "criterio personalizado 2",
+            data: [
+                { y: '2006', a: 100, b: 90, c: 100 },
+                { y: '2007', a: 75, b: 65, c: 100 },
+                { y: '2008', a: 50, b: 40, c: 100 },
+                { y: '2009', a: 75, b: 65, c: 100 },
+                { y: '2010', a: 50, b: 40, c: 100 },
+                { y: '2011', a: 75, b: 65, c: 100 },
+                { y: '2012', a: 100, b: 90, c: 100 }
+            ]
+        }
+    ];
+    return lineChartData;
+};
+
+function getBarChartData() {
+    var barChartData = [
+        {
+            id: "numPersonas",
+            name: "# de personas",
+            data: [
+                { y: '2006', a: 100, b: 90, c: 100 },
+                { y: '2007', a: 75, b: 65, c: 100 },
+                { y: '2008', a: 50, b: 40, c: 50 },
+                { y: '2009', a: 75, b: 65, c: 2 },
+                { y: '2010', a: 50, b: 40, c: 100 },
+                { y: '2011', a: 75, b: 65, c: 100 },
+                { y: '2012', a: 100, b: 90, c: 100 }
+            ]
+        },
+        {
+            id: "criterio1",
+            name: "criterio personalizado 1",
+            data: [
+                { y: '2006', a: 100, b: 90, c: 100 },
+                { y: '2007', a: 75, b: 65, c: 100 },
+                { y: '2008', a: 50, b: 40, c: 1 },
+                { y: '2009', a: 75, b: 65, c: 100 },
+                { y: '2010', a: 50, b: 40, c: 100 },
+                { y: '2011', a: 75, b: 65, c: 1 },
+                { y: '2012', a: 100, b: 90, c: 100 }
+            ]
+        },
+        {
+            id: "criterio2",
+            name: "criterio personalizado 2",
+            data: [
+                { y: '2006', a: 100, b: 90, c: 100 },
+                { y: '2007', a: 75, b: 65, c: 100 },
+                { y: '2008', a: 50, b: 40, c: 100 },
+                { y: '2009', a: 75, b: 65, c: 100 },
+                { y: '2010', a: 50, b: 40, c: 100 },
+                { y: '2011', a: 75, b: 65, c: 100 },
+                { y: '2012', a: 100, b: 90, c: 100 }
+            ]
+        }
+    ];
+    return barChartData;
+};
